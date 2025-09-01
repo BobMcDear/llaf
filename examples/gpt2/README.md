4ef7eb4cd0de5a0fd0f02f480b4c4dd252f969ef
Borna Ahmadzadeh <borna.ahz@gmail.com>
2025-09-01 12:00:15 -0400

    Refactor GPT-2 script

--- examples/gpt2/README.md
+++ examples/gpt2/README.md
@@ -2,19 +2,19 @@
 
 This example demonstrates how to invoke llaf from Python to produce text using GPT-2. It supports pre-trained Hugging Face models, whose parameters are appropriately reshaped for llaf on the first use and saved as `.npz` files. Please follow these instructions to run the model:
 
-1. Make sure the appropriate OpenCL runtime is installed on your system.
+1. Make sure the appropriate drivers are installed on your system.
 2. Navigate to this directory.
 3. Install the required dependencies: `pip install -r requirements.txt`.
-4. Compile `llm.fut` to a server-mode executable: `futhark cuda --server ../../src/llm.fut -o llm` (or use any other backend you wish, e.g. `opencl` or `hip`).
+4. Compile `llm.fut` to a server-mode executable: `futhark cuda --server ../../src/llm.fut -o llm`. You can use other backends instead of `cuda`, e.g., `opencl` or `hip`.
 5. Run the inference script with your desired prompt: `python main.py "Prompt goes here"`.
     * Arguments:
-    * `--name`: GPT-2 variant to use.
-        * Options are `gpt2`, `gpt2-medium`, `gpt2-large`, and `gpt2-xl`.
-    * `--cnt`: Number of additional tokens to generate.
-    * `--benchmark`: Flag for benchmarking llaf against Hugging Face.
-    * `--dump DIR`: dumps parameters and encoded prompt to the given directory as individual files in the Futhark binary data format.
+      * `--name`: GPT-2 variant to use.
+          * Options are `gpt2`, `gpt2-medium`, `gpt2-large`, and `gpt2-xl`.
+      * `--cnt`: Number of additional tokens to generate.
+      * `--benchmark`: Flag for benchmarking llaf against Hugging Face.
+      * `--dump DIR`: Flag for dumping Futhark-readable data files to this directory.
 
-With the initial prompt of `Once upon a time`, laff can be anywhere from 3x to 10x slower than the Hugging Face implementation of GPT-2 on an RTX 2070 GPU, as seen below. Despite its poor relative performance, the Futhark compiler still does a decent job, especially considering that it's aimed towards general array computations and isn't optimized for deep learning.
+With the initial prompt of `Once upon a time`, laff can be anywhere from 3x to 10x slower than the Hugging Face implementation of GPT-2 on an RTX 2070 GPU, as seen below. This benchmark was run using OpenCL, which was on par with the CUDA backend on this hardware. Despite its poor relative performance, the Futhark compiler still does a decent job, especially considering that it's aimed towards general array computations and isn't optimized for deep learning.
 
 <p align="center">
   <img src="perf.svg" alt="llaf vs Hugging Face performance" width="600"/>
--- examples/gpt2/main.py
+++ examples/gpt2/main.py
@@ -6,66 +6,85 @@ Runs GPT-2 inference using llaf.
 import argparse
 import os
 import time
+from collections.abc import Mapping
+from types import TracebackType
+from typing import List, Optional, Type
 
+import futhark_data
+import futhark_server
 import numpy as np
 import torch
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 
 from params import save_gpt2
 
-import futhark_data
-import futhark_server
-
-PARAM_NAMES = ['tok_emb', 'pos_emb', 'gamma1s', 'beta1s', 'gamma2s', 'beta2s', 'w_ins', 'b_ins', 'w_outs', 'b_outs', 'w1s', 'b1s', 'w2s', 'b2s', 'gamma', 'beta', 'w']
 
 class LLM:
-    def __init__(self, params):
-        self.server = futhark_server.Server('./llm', '--cache=llm.cache')
-        for v in PARAM_NAMES:
-            self.server.put_value(v, params[v])
-        self.server.cmd_call('init', 'params', *PARAM_NAMES)
-        for v in PARAM_NAMES:
-            self.server.cmd_free(v)
+    PARAM_NAMES: List[str] = ['tok_emb', 'pos_emb',
+                              'gamma1s', 'beta1s', 'gamma2s', 'beta2s',
+                              'w_ins', 'b_ins', 'w_outs', 'b_outs',
+                              'w1s', 'b1s', 'w2s', 'b2s',
+                              'gamma', 'beta', 'w']
 
-    def __enter__(self):
+    def __init__(self, params: Mapping[str, np.ndarray]) -> None:
+        self.server = futhark_server.Server('./llm', '--cache=llm.cache')
+        for name in self.PARAM_NAMES:
+            self.server.put_value(name, params[name])
+        self.server.cmd_call('init', 'params', *self.PARAM_NAMES)
+        for name in self.PARAM_NAMES:
+            self.server.cmd_free(name)
+
+    def __enter__(self) -> 'LLM':
         self.server.__enter__()
         return self
 
-    def __exit__(self, type, value, traceback):
-        return self.server.__exit(type, value, traceback)
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+        ) -> Optional[bool]:
+        return self.server.__exit(exc_type, exc_value, traceback)
 
-    def gen(self, ids, cnt: int):
+    def gen(self, ids: np.ndarray, cnt: int) -> np.ndarray:
         self.server.put_value('ids', ids)
         self.server.put_value('cnt', np.int64(cnt))
         self.server.cmd_call('gen', 'out', 'ids', 'params', 'cnt')
         out = self.server.get_value('out')
+
         self.server.cmd_free('out')
         self.server.cmd_free('ids')
         self.server.cmd_free('cnt')
+
         return out
 
-def main(text: str, name: str = 'gpt2', cnt: int = 20, bench: bool = False, dump: str | None = None) -> None:
+
+def main(
+    text: str,
+    name: str = 'gpt2',
+    cnt: int = 20,
+    bench: bool = False,
+    dump: Optional[str] = None,
+    ) -> None:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(script_dir, name + '.npz')
     if not os.path.exists(path):
         save_gpt2(name)
+
     params = np.load(path)
-
     tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
-
     ids = np.array(tokenizer.encode(text), dtype=np.int64)
 
     if dump is not None:
         print(f'Writing Futhark data files to {dump}.')
         os.makedirs(dump, exist_ok=True)
-        for v in PARAM_NAMES:
-            futhark_data.dump(params[v], open(f'{dump}/{v}.data', 'wb'), binary=True)
+        for name in LLM.PARAM_NAMES:
+            futhark_data.dump(params[name], open(f'{dump}/{name}.data', 'wb'),
+                              binary=True)
         futhark_data.dump(ids, open(f'{dump}/ids.data', 'wb'), binary=True)
 
     llaf = LLM(params)
-
     out = llaf.gen(ids, cnt)
-
     print('Output:', tokenizer.decode(out.tolist()))
 
     if bench:
@@ -75,11 +94,10 @@ def main(text: str, name: str = 'gpt2', cnt: int = 20, bench: bool = False, dump
         start = time.time()
         _ = llaf.gen(ids, cnt)
         end = time.time()
-        print(f'\nllaf time: {end - start:.3f} s')
+        print(f'\nllaf time: {end-start:.3f} s')
 
         torch.set_grad_enabled(False)
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
         hf = GPT2LMHeadModel.from_pretrained(name).to(device).eval()
         ids = torch.tensor(ids, device=device).unsqueeze(0)
         mask = torch.ones_like(ids)
@@ -96,7 +114,7 @@ def main(text: str, name: str = 'gpt2', cnt: int = 20, bench: bool = False, dump
         if device.type == 'cuda':
             torch.cuda.synchronize()
         end = time.time()
-        print(f'Hugging Face time: {end - start:.4f} s')
+        print(f'Hugging Face time: {end-start:.3f} s')
 
 
 if __name__ == '__main__':
@@ -119,7 +137,7 @@ if __name__ == '__main__':
     parser.add_argument('--dump',
                         type=str,
                         default=None,
-                        help='Dump Futhark-readable data files to this directory.')
+                        help='Flag for dumping Futhark-readable data files to this directory.')
 
     args = parser.parse_args()
     main(args.text, args.name, args.cnt, args.bench, args.dump)
